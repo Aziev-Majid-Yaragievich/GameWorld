c42d8d31104492f2a8f17d4c8df6b79749f6acf8
AzievMajid <52350178+AzievMajid@users.noreply.github.com>
2020-01-16 12:21:31 +0300

    One
    One

--- inv.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$db = new mysqli('localhost', 'root', '', 'daisy');
-
-$q = $db->query("UPDATE USERS SET INVENTORY  = '" . $_POST['inventory'] . "' WHERE TICKET = '" . $_POST['ticket'] ."';");
--- logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-unset($_SESSION["roleflags"]);
-unset($_SESSION["ticket"]);
-unset($_SESSION["userId"]);
-header("Location: /");
